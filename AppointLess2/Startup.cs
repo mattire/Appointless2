5d65251be6d27d63be10812e15a24448c1b79557
mattire <alr2d4@hotmail.com>
2018-12-06 18:54:33 +0200

    Add project files.

---
+++ AppointLess2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppointLess2.Startup))]
+namespace AppointLess2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
